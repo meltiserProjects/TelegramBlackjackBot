3a78c22a0608772b4cb3895988d6cd7691c457a0
meltiser <dmitriy-95@yandex.ru>
2018-09-17 13:09:41 +0300

    class main refactored

--- src/main/java/ru/grigorev/telegram/blackjack/Main.java
+++ src/main/java/ru/grigorev/telegram/blackjack/Main.java
@@ -16,30 +16,32 @@ import java.util.logging.Level;
  */
 public class Main {
     private static final String LOGTAG = "MAIN";
-
-    static {
-        ApiContextInitializer.init();
-    }
-
-    private static LongPollingBot bot = new Bot();
+    private static LongPollingBot bot;
 
     public static void main(String[] args) {
+        loggerInitialization();
+        botInitialization();
+    }
+
+    private static void botInitialization() {
+        ApiContextInitializer.init();
+        bot = new Bot();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        try {
+            BotLogger.info(LOGTAG, "Try initialization...");
+            botsApi.registerBot(bot);
+            BotLogger.info(LOGTAG, "Bot is running...");
+        } catch (TelegramApiException e) {
+            BotLogger.severe(LOGTAG, e);
+        }
+    }
+
+    private static void loggerInitialization() {
         BotLogger.setLevel(Level.ALL);
         BotLogger.registerLogger(new ConsoleHandler());
         try {
             BotLogger.registerLogger(new BotsFileHandler());
         } catch (IOException e) {
-            BotLogger.severe(LOGTAG, e);
-        }
-        botInitialization();
-    }
-
-    private static void botInitialization() {
-        TelegramBotsApi botsApi = new TelegramBotsApi();
-        try {
-            botsApi.registerBot(bot);
-            System.out.println("Bot is running...");
-        } catch (TelegramApiException e) {
             BotLogger.severe(LOGTAG, e);
         }
     }
